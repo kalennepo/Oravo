8ba1d6002fb186b56747c756e1adcaf2828fbfbd
NepoNemo <kalen.nepomuceno@gmail.com>
2025-08-21 09:17:08 -0500

    add Spring Boot scaffold

---
+++ backend/audiobook-api/src/main/java/com/kalen/audiobook/AudiobookApiApplication.java
@@ -0,0 +1,13 @@
+package com.kalen.audiobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiobookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiobookApiApplication.class, args);
+	}
+
+}
---
+++ backend/audiobook-api/src/test/java/com/kalen/audiobook/AudiobookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalen.audiobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiobookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
