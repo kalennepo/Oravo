b192a6b968f02a4b5adb0df808ade66cb52221a5
kalen <kalen.nepomuceno@gmail.com>
2025-08-27 18:07:55 -0500

    Add test API endpoint

---
+++ backend/audiobook-api/src/main/java/com/kalen/audiobook/HelloController.java
@@ -0,0 +1,13 @@
+package com.kalen.audiobook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "audiobook-api up and running";
+    }
+}
